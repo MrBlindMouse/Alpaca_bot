4d63c237d890970781fcb4039a2cbe706b5ce355
MrBlindMouse <neljohan1206@gmail.com>
2024-10-04 15:56:34 +0200

    Spelling mistake

--- bot.py
+++ bot.py
@@ -143,7 +143,7 @@ def bot():
                             create_order(balance_value,"buy",equity["symbol"])
                 print_str = ""
                 for key,value in enumerate(account):
-                    print_str += str(value)+":"+str(trunc(account[value]*100),1)+"% | "
+                    print_str += str(value)+":"+str(trunc(account[value]*100,1))+"% | "
                 print(" "*150, end="\r", flush=True)
                 print(print_str, end="\r", flush=True)
                 time.sleep(10)
